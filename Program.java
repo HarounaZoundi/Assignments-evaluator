90c07a4798f8d11f60c9bd6486190a74d063ed50
Harouna Zoundi <harouna.zoundi1@gmail.com>
2020-05-06 17:21:13 -0400

    Adding makefile

--- Program.java
+++ Program.java
@@ -5,19 +5,19 @@ public class Program {
    static private ArrayList<Assignment> assignments = new ArrayList<>() ;
    
    public void newAssignment(String str){
-     Assignment newAssign = new Assignment(str);
-     boolean status = false;
-     for(int i=0; i< assignments.size(); i++){
-            if(assignments.get(i).getIdentifier().equals(newAssign.getIdentifier())){
-               status = true;
-               assignments.add(i, newAssign);
-               break;
-            }
-            
+      Assignment newAssign = new Assignment(str);
+      boolean status = false;
+      for(int i=0; i< assignments.size(); i++){
+         if(assignments.get(i).getIdentifier().equals(newAssign.getIdentifier())){
+            status = true;
+            assignments.add(i, newAssign);
+            break;
          }
+            
+      }
       if(!status){
          assignments.add(newAssign);
-       }
+      }
    }
    public static int getExpression(String identifier){
       int x=0;
---
+++ makefile
@@ -0,0 +1,27 @@
+
+JCC = javac
+JC = java
+JFLAGS = -g
+
+# typing 'make' will invoke the first target entry in the makefile 
+# (the default one in this case)
+#
+
+Assignment.class: Assignment.java
+	$(JCC) $(JFLAGS) Assignment.java
+
+ExpEvaluator.class: ExpEvaluator.java
+	$(JCC) $(JFLAGS) ExpEvaluator.java
+
+Identifier.class: Identifier.java
+	$(JCC) $(JFLAGS) Identifier.java
+
+Program.class: Program.java
+	$(JCC) $(JFLAGS) Program.java
+
+Main.class: Main.java
+	$(JCC) $(JFLAGS) Main.java
+run: Main.class
+	$(JC) Main.class
+clean: 
+	$(RM) *.class
